1882c5d80db700d4609ce19510fde22a0efa094a
Kiran Tambe <kiran@anlyz.io>
2017-11-12 12:09:11 +0300

    Added slot to override template of aside.

--- docs/example/asideDocs.vue
+++ docs/example/asideDocs.vue
@@ -3,6 +3,7 @@
     <div class="bs-example">
       <button class="btn btn-danger btn-lg" @click="showLeft = true">Show Aside on left</button>
       <button class="btn btn-success btn-lg" @click="showRight = true">Show Aside on right</button>
+      <button class="btn btn-success btn-lg" @click="showCustom = true">Show Aside with custom body</button>
 
       <sidebar :show="showLeft" @close="showLeft = false" placement="left" header="Title" :width="350">
         <h4>Text in aside</h4>
@@ -36,6 +37,33 @@
           <button type="button" class="btn btn-default" @click="showRight=false">Close</button>
         </div>
       </sidebar>
+      <sidebar :show="showCustom" @close='showCustom = false' placement="right" header="Title" :width="350">
+        <div slot="aside-dialog">
+          <tabs :nav-style="navStyle" :justified="justified">
+            <tab header="zero">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </tab>
+            <tab header="one">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </tab>
+            </tab-group>
+          </tabs>
+        </div>
+      </sidebar>
     </div>
     <doc-code language="markup">
       &lt;sidebar :show="showRight" placement="right" header="Title" width="350">
@@ -43,6 +71,11 @@
       &lt;/sidebar>
       &lt;sidebar :show="showLeft" placement="left" header="Title" width="350">
         ...
+      &lt;/sidebar>
+      &lt;sidebar :show="showCustom" placement="right" header="Title" width="350">
+        &lt;div slot="aside-dialog">
+        ...
+        &lt;/div>
       &lt;/sidebar>
     </doc-code>
     <doc-table>
@@ -79,18 +112,25 @@ import docSection from './utils/docSection.vue'
 import docTable from './utils/docTable.js'
 import docCode from './utils/docCode.js'
 import sidebar from 'src/Aside.vue'
+import tabs from 'src/Tabs.vue'
+import tabGroup from 'src/TabGroup.vue'
+import tab from 'src/Tab.vue'
 
 export default {
   components: {
     docSection,
     docTable,
     docCode,
-    sidebar
+    sidebar,
+    tabs,
+    tabGroup,
+    tab
   },
   data () {
     return {
       showLeft: false,
-      showRight: false
+      showRight: false,
+      showCustom: false
     }
   }
 }
